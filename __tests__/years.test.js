861fc2dc7ccf5575437b1c5dc0094bc0e4dce4ff
Tessa Hayden <tessahayden3@gmail.com>
2023-07-27 12:30:42 -0700

    Red light test for mercuryYear()

--- __tests__/years.test.js
+++ __tests__/years.test.js
@@ -5,4 +5,9 @@ describe('Years', () => {
         const years = new Years("mercury", "venus", "mars", "jupiter", "earth");
         expect(years).toEqual({"mercury":"mercury", "venus": "venus", "mars": "mars", "jupiter": "jupiter", "earth": "earth"});
     });
+    test('it should calculate a Mercury year compared to an Earth year.', () => {
+        const years = new Years("mercury", "venus", "mars", "jupiter", "earth");
+        let eYear = 3;
+        expect(years.mercuryYear(eYear)).toEqual(12.5);
+    })
 })
