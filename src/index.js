ca3b23c42a40eb1926086eae5a86371f09642a12
Tessa Hayden <tessahayden3@gmail.com>
2023-08-05 23:07:34 -0700

    Finish the UI to display the user's age per planet.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,75 @@
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/styles.css";
+import Year from "../src/years";
+
+function calcMerc(currentAge) {
+  let year = new Year();
+  return year.mercuryYear(currentAge);
+}
+function calcVenus(currentAge) {
+  let year = new Year();
+  return year.venusYear(currentAge);
+}
+function calcMars(currentAge) {
+  let year = new Year();
+  return year.marsYear(currentAge);
+}
+function calcJupiter(currentAge) {
+  let year = new Year();
+  return year.jupiterYear(currentAge);
+}
+function handleMercury() {
+  const age = document.querySelector("#currAgeInput").value;
+  const calculatedAge = calcMerc(age);
+  return calculatedAge;
+}
+function displayMercuryAges(event) {
+  event.preventDefault();
+  const calculateAge = handleMercury();
+  const mercuryDiv = document.querySelector("#merc-age");
+  mercuryDiv.innerText = "Your age in Mercury years: " + calculateAge;
+}
+function handleVenus() {
+  const age = document.querySelector("#currAgeInput").value;
+  const calculatedAge = calcVenus(age);
+  return calculatedAge;
+}
+function displayVenusAges(event) {
+  event.preventDefault();
+  const calculateAge = handleVenus();
+  const venusDiv = document.querySelector("#ven-age");
+  venusDiv.innerText = "Your age in Venus years: " + calculateAge;
+}
+function handleMars() {
+  const age = document.querySelector("#currAgeInput").value;
+  const calculatedAge = calcMars(age);
+  return calculatedAge;
+}
+function displayMarsAges(event) {
+  event.preventDefault();
+  const calculateAge = handleMars();
+  const marsDiv = document.querySelector("#mars-age");
+  marsDiv.innerText = "Your age in Mars years: " + calculateAge;
+}
+function handleJupiter() {
+  const age = document.querySelector("#currAgeInput").value;
+  const calculatedAge = calcJupiter(age);
+  return calculatedAge;
+}
+function displayJupiterAges(event) {
+  event.preventDefault();
+  const calculateAge = handleJupiter();
+  const jupiterDiv = document.querySelector("#jupiter-age");
+  jupiterDiv.innerText = "Your age in Jupiter years: " + calculateAge;
+}
+window.addEventListener("load", function () {
+  const mercBtn = document.querySelector("#mercury-btn");
+  const venusBtn = document.querySelector("#venus-btn");
+  const marsBtn = document.querySelector("#mars-btn");
+  const jupiterBtn = document.querySelector("#jupiter-btn");
+  mercBtn.addEventListener("click", displayMercuryAges);
+  venusBtn.addEventListener("click", displayVenusAges);
+  marsBtn.addEventListener("click", displayMarsAges);
+  jupiterBtn.addEventListener("click", displayJupiterAges);
+});
--- src/years.js
+++ src/years.js
@@ -1,47 +1,47 @@
 export default class Years {
-    constructor(mercury, venus, mars, jupiter, earth) {
-        this.mercury = mercury;
-        this.venus = venus;
-        this.mars = mars;
-        this.jupiter = jupiter;
-        this.earth = earth
-    }
-    mercuryYear(eYear) {
-        return ((eYear * 365) / 87.6).toFixed(2);
-    }
-    venusYear(eYear) {
-        return ((eYear * 365) / 226.3).toFixed(2);
-    }
-    marsYear(eYear) {
-        return ((eYear * 365) * 1.88).toFixed(2);
-    }
-    jupiterYear(eYear) {
-        return ((eYear * 365) * 11.86).toFixed(2);
-    }
-    planetAges(currentAge, pastAge) {
-        let ageDiff = currentAge - pastAge;
-        let ageArray = [];
-        let mercuryAge = this.mercuryYear(ageDiff);
-        let venusAge = this.venusYear(ageDiff);
-        let marsAge = (ageDiff / 1.88).toFixed(2);
-        let jupiterAge = (ageDiff / 11.86).toFixed(2);
-        ageArray.push(mercuryAge);
-        ageArray.push(venusAge);
-        ageArray.push(marsAge);
-        ageArray.push(jupiterAge);
-        return ageArray;
-    }
-    futureAge(currentAge, futureAge) {
-        let ageDiff = futureAge - currentAge;
-        let ageArray = [];
-        let mercuryAge = this.mercuryYear(ageDiff);
-        let venusAge = this.venusYear(ageDiff);
-        let marsAge = (ageDiff / 1.88).toFixed(2);
-        let jupiterAge = (ageDiff / 11.86).toFixed(2);
-        ageArray.push(mercuryAge);
-        ageArray.push(venusAge);
-        ageArray.push(marsAge);
-        ageArray.push(jupiterAge);
-        return ageArray;
-    }
+  constructor(mercury, venus, mars, jupiter, earth) {
+    this.mercury = mercury;
+    this.venus = venus;
+    this.mars = mars;
+    this.jupiter = jupiter;
+    this.earth = earth;
+  }
+  mercuryYear(eYear) {
+    return ((eYear * 365) / 87.6).toFixed(2);
+  }
+  venusYear(eYear) {
+    return ((eYear * 365) / 226.3).toFixed(2);
+  }
+  marsYear(eYear) {
+    return (eYear * 1.88).toFixed(2);
+  }
+  jupiterYear(eYear) {
+    return (eYear * 11.86).toFixed(2);
+  }
+  planetAges(currentAge, pastAge) {
+    let ageDiff = currentAge - pastAge;
+    let ageArray = [];
+    let mercuryAge = this.mercuryYear(ageDiff);
+    let venusAge = this.venusYear(ageDiff);
+    let marsAge = (ageDiff / 1.88).toFixed(2);
+    let jupiterAge = (ageDiff / 11.86).toFixed(2);
+    ageArray.push(mercuryAge);
+    ageArray.push(venusAge);
+    ageArray.push(marsAge);
+    ageArray.push(jupiterAge);
+    return ageArray;
+  }
+  futureAge(currentAge, futureAge) {
+    let ageDiff = futureAge - currentAge;
+    let ageArray = [];
+    let mercuryAge = this.mercuryYear(ageDiff);
+    let venusAge = this.venusYear(ageDiff);
+    let marsAge = (ageDiff / 1.88).toFixed(2);
+    let jupiterAge = (ageDiff / 11.86).toFixed(2);
+    ageArray.push(mercuryAge);
+    ageArray.push(venusAge);
+    ageArray.push(marsAge);
+    ageArray.push(jupiterAge);
+    return ageArray;
+  }
 }
